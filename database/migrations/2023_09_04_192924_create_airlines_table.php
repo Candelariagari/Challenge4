19fc75a73672a54aea1fc4f93c9ccfd2b6e0fb72
Candelariagari <Candelaria.gari@lightit.io>
2023-09-11 15:30:51 -0300

    feat: Delete airlines

--- database/migrations/2023_09_04_192924_create_airlines_table.php
+++ database/migrations/2023_09_04_192924_create_airlines_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('description')->nullable();
             $table->timestamps();
+            // $table->foreignId('city_id')->constrained('cities')->cascadeOnDelete();
             $table->softDeletes();
         });
     }
